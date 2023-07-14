f822453a887cbf3ef5ecf6af83a48a7a0a8f6412
Seung Hyun Choi <choi772@data.cs.purdue.edu>
2023-07-14 06:58:12 -0400

    ch2 vector.cpp added

---
+++ ch2/linear_ds/vector.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int arr[5] = {3,3,3};
+  vector<int> v(5, 5);
+  printf("arr[2] = %d and v[2] = %d\n", arr[2], v[2]);
+  
+
+  return 0;
+}
